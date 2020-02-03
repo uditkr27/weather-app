6ff33e575203df492338d5c186e906091ae1da47
uditkr27 <utkmr27jun@gmail.com>
2020-02-04 00:55:50 +0530

    form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const API_KEY = "cc9bbcbce7710aaabe8f56065e3c843f";
 
+class App extends React.Component {
+  getWeather = async (e) => {
+    e.preventDefault();
+    const api_call = await fetch();
+    const data = await api_call.json();
+    console.log(data);
+  }
+  render() {
+    return( 
+      <div>
+        <Titles />
+        <Form getWeather={this.getWeather}/>
+        <Weather />
+      </div>
+    );
+  }
+
+};
 export default App;
---
+++ src/components/Weather.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Weather extends React.Component {
+    render(){
+        return(
+            <div>Weather Components</div>
+        );
+    }
+
+};
+export default Weather;
