11853b06330d3b7309086bdbe9457bf7ad40676c
uditkr27 <utkmr27jun@gmail.com>
2020-02-11 22:28:56 +0530

    weather

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,24 @@
 import React from "react";
 
-class Form extends React.Component {
-    render(){
-        return(
-            <form onSubmit={this.props.getWeather }>
+const Form = props => (
+    <form onSubmit={props.getWeather }>
                 <input type="text" name="city" placeholder="city..." />
                 <input type="text" name="country" placeholder="country..." />
                 <button>Get Weather</button>
             </form>
-        );
-    }
-};
+);
+
+
+// class Form extends React.Component {
+//     render(){
+//         return(
+//             <form onSubmit={this.props.getWeather }>
+//                 <input type="text" name="city" placeholder="city..." />
+//                 <input type="text" name="country" placeholder="country..." />
+//                 <button>Get Weather</button>
+//             </form>
+//         );
+//     }
+// };
 
 export default Form;
--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,13 +1,20 @@
 import React from "react";
 
-class Titles extends React.Component {
-    render() {
-        return (
-            <div>
+const Titles = () => (
+    <div>
                 <h1>Weather Finder</h1>
                 <p>Find out temperature, conditions and more ....</p>
             </div>
-        );
-    }
-};
+);
+
+// class Titles extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Weather Finder</h1>
+//                 <p>Find out temperature, conditions and more ....</p>
+//             </div>
+//         );
+//     }
+// };
 export default Titles;
